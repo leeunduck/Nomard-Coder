21712ff81409f8f65a01b79cab1fc27944ee55eb
HyunseokLEE <unducklee@gmail.com>
2022-10-22 13:28:57 +0900

    update 10/21

---
+++ react-for-beginners/src/App_2.js
@@ -0,0 +1,14 @@
+import { useState, useEffect } from "react";
+
+function App() {
+  const [counter, setValue] = useState(0);
+  const onClick = () => setValue((prev) => prev + 1);
+  console.log("call an api");
+  return (
+    <div>
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click me</button>
+    </div>
+  );
+}
+export default App;

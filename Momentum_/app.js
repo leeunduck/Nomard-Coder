57ccdabf5f985675adc23e87f7de9d906ea6c2bb
HyunseokLEE <unducklee@gmail.com>
2022-11-04 23:58:43 +0900

    update 11/4

--- Momentum_/app.js
+++ Momentum_/app.js
@@ -1,3 +1,12 @@
-let amIStrong;
+const player = {
+  name: "Lee",
+  points: 9999,
+  handsome: true,
+  fat: false,
+};
 
-console.log(amIStrong);
+console.log(player);
+
+player.attackPoint = 98;
+
+console.log(player);

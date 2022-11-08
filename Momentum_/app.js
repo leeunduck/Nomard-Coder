7c0dcdea1f6340e7d56d62c2a07b14396f212b0f
HyunseokLEE <unducklee@gmail.com>
2022-11-08 18:24:13 +0900

    update 11/8

--- Momentum_/app.js
+++ Momentum_/app.js
@@ -1,29 +1,11 @@
-const calculator = {
-  plus: function (a, b) {
-    return a + b;
-  },
-  minus: function (a, b) {
-    return a - b;
-  },
-  times: function (a, b) {
-    return a * b;
-  },
-  divide: function (a, b) {
-    return a / b;
-  },
-  power: function (a, b) {
-    return a ** b;
-  },
-};
+const age = parseInt(prompt("How old are you?"));
 
-const plusResults = calculator.plus(2, 3);
-const minusResults = calculator.minus(plusResults, 3);
-const timesResults = calculator.times(2, minusResults);
-const divideResults = calculator.divide(timesResults, 3);
-const powerResults = calculator.power(2, divideResults);
-
-console.log(plusResults);
-console.log(minusResults);
-console.log(timesResults);
-console.log(divideResults);
-console.log(powerResults);
+if (isNaN(age) || age < 0) {
+  console.log("Please write a real positive number");
+} else if (age <= 18) {
+  console.log("You are too young");
+} else if (age >= 18 && age <= 50) {
+  console.log("You can hard drink");
+} else {
+  console.log("You can drink");
+}
